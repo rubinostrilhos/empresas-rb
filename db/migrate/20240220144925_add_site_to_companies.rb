122455394ef80846a8916c6045a7f234caab7b4d
anthony vieira <thony16@gmail.com>
2024-02-20 11:50:53 -0300

    feat: add site, uf e category

---
+++ db/migrate/20240220144925_add_site_to_companies.rb
@@ -0,0 +1,5 @@
+class AddSiteToCompanies < ActiveRecord::Migration[7.1]
+  def change
+    add_column :companies, :status, :string
+  end
+end
---
+++ db/migrate/20240220144957_add_uf_to_companies.rb
@@ -0,0 +1,5 @@
+class AddUfToCompanies < ActiveRecord::Migration[7.1]
+  def change
+    add_column :companies, :status, :string
+  end
+end
