51086fdf3b0fcd41ffc40c6bb28b2e121349b335
Caio Alves <caioalvesfig@gmail.com>
2024-02-20 18:57:14 -0300

    create favorite model and fixed home button on the navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_20_144957) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_20_211224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_20_144957) do
     t.string "uf"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_favorites_on_company_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,4 +53,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_20_144957) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "companies"
+  add_foreign_key "favorites", "users"
 end
