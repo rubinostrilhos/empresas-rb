7f25db4d8122723114db345a32de6162a7cfbfd3
CaioBrasetti <caiobrasetti@hotmail.com>
2024-02-26 17:09:02 -0300

    Implementando o search

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,4 +5,11 @@ class Company < ApplicationRecord
   validates :name, :phone, :segment,
             :status, :site, :category, :uf, presence: true
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_email,
+    against: [ :name, :email ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
