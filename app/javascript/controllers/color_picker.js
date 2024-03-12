ac7803277d4dc3a2884e39e33299140739da7ee3
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2024-03-12 14:24:59 -0300

    card com componentes do for de new company

---
+++ app/javascript/controllers/color_picker.js
@@ -0,0 +1,19 @@
+// let colorInputSelected = document.getElementById('color-card-selected');
+
+// colorInputSelected.addEventListener('input', function() {
+
+//     let selectedColor = colorInputSelected.value;
+
+//     colorInputSelected.value = selectedColor;
+
+//     console.log(colorInputSelected);
+//     console.log(typeof colorInputSelected);
+// });
+
+
+let colorInputSelected = document.getElementById('color-card-selected');
+
+colorInputSelected.addEventListener('input', function() {
+    let selectedColor = colorInputSelected.value;
+    console.log(selectedColor);
+});
