78bb1f5d97e54d23f06f186c78a0a7fd797b1e40
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2024-03-14 18:51:52 -0300

    card e pagina de empresas funcionando com responsividade parcial

--- app/javascript/controllers/color_picker.js
+++ app/javascript/controllers/color_picker.js
@@ -1,19 +1,9 @@
-// let colorInputSelected = document.getElementById('color-card-selected');
-
-// colorInputSelected.addEventListener('input', function() {
-
-//     let selectedColor = colorInputSelected.value;
-
-//     colorInputSelected.value = selectedColor;
-
-//     console.log(colorInputSelected);
-//     console.log(typeof colorInputSelected);
-// });
-
 
 let colorInputSelected = document.getElementById('color-card-selected');
 
 colorInputSelected.addEventListener('input', function() {
     let selectedColor = colorInputSelected.value;
-    console.log(selectedColor);
+
+    let evento = new CustomEvent('selectedColorChanged', { detail: selectedColor });
+    window.dispatchEvent(evento);
 });
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,8 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
