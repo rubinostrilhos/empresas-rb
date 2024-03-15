e7d64ab90648774e2eee963c46390bcd8c90f13b
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2024-03-15 17:19:31 -0300

    card e pagina de empresas funcionando com responsividade total

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.hosts << ".ngrok-free.app"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
