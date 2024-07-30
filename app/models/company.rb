92cf21e6975070b569d61acf9ffbaf4625f16be6
Victor Campos <victorcampos@Victors-MacBook-Pro.local>
2024-07-30 07:59:33 -0300

    feat: add tests to companis index controller

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,7 +3,12 @@ class Company < ApplicationRecord
   SEGMENTS = %w[Agro IT Finance Transport Marketing Food Tourism Healthcare Education
                 Recruitment Fashion Media Fitness Service Retail Construction].sort
 
+  VISIBLE_STATUSES = %w[Ativo Pendente].freeze
+
   scope :search_by_name_and_email, ->(query) { where('lower(name) LIKE ? OR lower(email) LIKE ?', "%#{query.downcase}%", "%#{query.downcase}%") }
+  scope :approved, -> { where(approval_status: true) }
+  scope :with_visible_status, -> { where(status: VISIBLE_STATUSES) }
+  scope :visible, -> { approved.with_visible_status }
 
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
@@ -13,6 +18,6 @@ class Company < ApplicationRecord
             :status, :site, :category, :uf, presence: true
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
   # validações para definir status de aprovação do cadastro da empresa por adm.
-  validates :status, inclusion: { in: %w[Ativo Inativo Pendente] }
+  validates :status, inclusion: { in: %w[Inativo] | VISIBLE_STATUSES }
   # validates :approval_status, inclusion: { in: [true, false] }reloa
 end
--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class CompaniesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test "Should redirect to login if user is not logged in" do
+    get companies_path
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'Should list a list of approved companies' do
+    user = users(:user)
+    sign_in user
+    get companies_path
+    assert_response :success
+    assert_select 'h1', 'EMPRESAS'
+    assert_select '.companies-total', "Total de empresas: #{Company.visible.count}"
+  end
+
+  test 'Should filter companies with query' do
+    user = users(:user)
+    sign_in user
+    get companies_path, params: { query: 'Two' }
+    assert_response :success
+    assert_select '.companies-total', "Total de empresas: 1"
+  end
 end
